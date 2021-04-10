1ef66d03ffd07948ca7ca43badb45dc79dd17078
imabiodun <abiodun0075@gmail.com>
2021-04-10 21:56:54 +0100

    okayed

---
+++ js/index.js
@@ -0,0 +1,9 @@
+navLink=document.getElementById("home")
+
+const goto= function(navLink,landhere){
+    return document.getElementById(landhere);
+
+}
+getLink=document.getElementsByClassName("nav-bar");
+var enterHTML=document.createTextNode("a");
+enterHTML.appendChild("link");
